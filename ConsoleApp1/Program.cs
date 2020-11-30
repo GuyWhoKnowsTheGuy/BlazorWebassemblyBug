adebe61a436c1b0aeb59f8a5749a6e411f400578
Jerome Bell <jeromebell@gmail.com>
2020-11-30 12:09:06 -0600

    Init;

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+using System.Net.Http;
+using System.ServiceModel.Syndication;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            using var http = new HttpClient();
+
+            await Logic.App.InvokeBugAsync(http);
+        }
+    }
+}
---
+++ Shared/App.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using System.Net.Http;
+using System.ServiceModel.Syndication;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace Logic
+{
+    public class App
+    {
+        public static async Task InvokeBugAsync(HttpClient _http)
+        {
+            try
+            {
+                XmlReader reader = XmlReader.Create(await _http.GetStreamAsync("https://winappmerit.azurewebsites.net/api/CorsProxy?feed=https://rss.nytimes.com/services/xml/rss/nyt/US.xml"));
+
+                SyndicationFeed feed = SyndicationFeed.Load(reader);
+
+                var att = feed.ElementExtensions.ReadElementExtensions<XElement>("pubDate", string.Empty);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error - {ex.Message}");
+            }
+        }
+    }
+}
